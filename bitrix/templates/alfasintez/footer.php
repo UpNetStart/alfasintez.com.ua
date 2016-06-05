448abd8aaa6b1ded16c18401be1feeb08da7db9d
uarealism <alfasintez@ex.ua>
2016-06-06 01:30:53 +0300

    init tamplate/

---
+++ bitrix/templates/alfasintez/footer.php
@@ -0,0 +1,23 @@
+<div id="footer-wrapper" class="container">
+    <div class="row">
+        <div class="phone-wrapper">
+            <ul>
+                <li class="phone">+38 (097) 296 61 06</li>
+                <li class="phone">+38 (0562) 35 00 26</li>
+                <li class="phone">+38 (067) 631 32 23</li>
+            </ul>
+        </div>
+
+
+        <div class="contacts-wrapper">
+            <ul>
+                <li class="contacts">E-MAIL:info@alfa-sintez.com.ua                             </li>
+                <li class="contacts">АДРЕС:ул. Набережная Ленина, 27а, оф.16 (вход с Московской)</li>
+            </ul>
+        </div>
+    </div>
+    <div class="row copy"> Copyright © 2016. Компания Alfasintez. Полное или частичное копирование материалов запрещено. </div>
+</div >
+</div>
+</body>
+</html>
---
+++ bitrix/templates/alfasintez/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/bootstrap-theme.css">
+<link rel="stylesheet" href="css/main.css">
+
+<script href="js/bootstrap.js"></script>
+<script href="js/npm.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<head>
+    <meta charset="UTF-8">
+    <title>ООО Альфа Синтез</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
+<body>
+<div class="wrapper clearfix">
+    <div id="header-wrapper" class="container">
+        <div class="row top-items col-lg-12">
+            <div class="lang-wrapper bluer col-lg-3">
+                <a class="bluer-link" href="#">Русский(RU)</a>
+                <a class="bluer-link" href="#">English(EN)</a>
+            </div>
+
+            <div class="call-back bluer col-lg-3">
+                <a class="bluer-link" href="#"> Заказ звонка менеджера. </a>
+            </div>
+
+            <div class="cart-wrapper bluer col-lg-2">
+
+            </div>
+        </div>
+
+        <div class="row header-items clearfix">
+            <div class="logo-wrapper col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                <img src="images/logo.png" alt="ООО Альфа Синтез" id="logo">
+            </div>
+
+            <div class="phone-wrapper col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                <ul>
+                    <li class="phone">+38 (097) 296 61 06</li>
+                    <li class="phone">+38 (0562) 35 00 26</li>
+                    <li class="phone">+38 (067) 631 32 23</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row-fluid menu-items clearfix">
+            <div class="row top_menu">
+                <ul>
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">Продукция</a></li>
+                    <li><a href="#">Пленки ПЭТФ</a></li>
+                    <li><a href="#">Упаковка яиц</a></li>
+                    <li><a href="#">Блистерная упаковка</a></li>
+                    <li><a href="#">О компании</a></li>
+                    <li><a href="#">Новости</a></li>
+                    <li><a href="#">Контакты</a></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
